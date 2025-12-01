59e90595d104be6151b9742dfbdd15a2a75a2cd3
Tristan North <git@tristan-north.com>
2025-12-01 17:43:57 +1100

    Day01 Part One solution.

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,3 +1,24 @@
 import 'dart:io';
 
-void main() {}
+enum dir { left, right }
+
+void main() {
+  final lines = File('input/day01_input.txt').readAsLinesSync();
+
+  var dialPos = 50;
+  var solution = 0;
+
+  for (final line in lines) {
+    final direction = line[0] == 'L' ? dir.left : dir.right;
+    final offset = int.parse(line.substring(1));
+
+    dialPos = switch (direction) {
+      .left => (dialPos - offset) % 100,
+      .right => (dialPos + offset) % 100,
+    };
+
+    if (dialPos == 0) solution++;
+  }
+
+  print("Day01 Solution: $solution");
+}
