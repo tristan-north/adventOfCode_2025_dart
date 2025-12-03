82cae43b4138211b1b40e477b863d955a73eb8a4
Tristan North <git@tristan-north.com>
2025-12-03 22:37:04 +1100

    Day03 Part One setup

---
+++ solutions/day03.dart
@@ -0,0 +1,11 @@
+// 1) Loop through and find the index of the largest number,
+// exclude the final digit. (fold?)
+
+// 2) Starting from that index, loop through again to find the
+// largest number for the second digit of the solution for that row.
+
+// 3) Sum each row solution for final solution.
+
+void main() {
+  print('day03');
+}
