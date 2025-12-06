6d44fdce4123f90437deab8cd0d412841fc7f594
Tristan North <git@tristan-north.com>
2025-12-06 14:57:09 +1100

    Day03 Part Two solution

--- solutions/day03.dart
+++ solutions/day03.dart
@@ -10,10 +10,13 @@
 
 import 'dart:io';
 
+const nBatteries = 12;
+
 void main() {
   final batteryBanks = File('input/day03_input.txt').readAsLinesSync();
 
   partOne(batteryBanks);
+  partTwo(batteryBanks);
 }
 
 void partOne(List<String> batteryBanks) {
@@ -26,11 +29,36 @@ void partOne(List<String> batteryBanks) {
     final second = chars.sublist(largest.$1 + 1).indexed.reduce(largestChar);
 
     final joltage = int.parse('${largest.$2}${second.$2}');
-    print('$bank : $joltage');
     solution += joltage;
   }
 
   print('Part One solution: $solution');
+}
+
+void partTwo(List<String> batteryBanks) {
+  var solution = 0;
+
+  for (final bank in batteryBanks) {
+    final chars = bank.split('');
+
+    var bankChars = '';
+    var startIdx = 0;
+
+    for (var i = 0; i < nBatteries; ++i) {
+      final largest = chars
+          .sublist(startIdx, bank.length - (11 - i))
+          .indexed
+          .reduce(largestChar);
+
+      startIdx += largest.$1 + 1;
+      bankChars += largest.$2;
+    }
+
+    final joltage = int.parse(bankChars);
+    solution += joltage;
+  }
+
+  print('Part Two solution: $solution');
 }
 
 // Return largest of a or b based on $2 converted to an int.
