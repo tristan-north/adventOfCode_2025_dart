c7d31042312cba7306cd0fb64fe863ed2e3a6bfa
Tristan North <git@tristan-north.com>
2025-12-11 15:46:05 +1100

    Day06 Part Two solution

--- solutions/day06.dart
+++ solutions/day06.dart
@@ -4,7 +4,47 @@ void main() {
   final input = File('input/day06_input.txt').readAsLinesSync();
 
   print('Day06 Part One solution: ${partOne(input)}');
+  print('Day06 Part Two solution: ${partTwo(input)}');
 }
+
+int partTwo(List<String> input) {
+  final rows = [...input];
+  final opRow = rows.removeLast();
+
+  var solution = 0;
+
+  var opFunc = opSum;
+  final valuesBuffer = <int>[];
+  for (var colIdx = 0; colIdx < opRow.length; ++colIdx) {
+    opFunc = switch (opRow[colIdx]) {
+      '+' => opSum,
+      '*' => opProduct,
+      _ => opFunc,
+    };
+
+    switch (getIntFromColumn(colIdx, rows)) {
+      case null:
+        solution += valuesBuffer.reduce(opFunc);
+        valuesBuffer.clear();
+        continue;
+
+      case int val:
+        valuesBuffer.add(val);
+    }
+  }
+  solution += valuesBuffer.reduce(opFunc); // For the last group of columns
+
+  return solution;
+}
+
+int? getIntFromColumn(colIdx, List<String> rows) {
+  final intStr =
+      '${rows[0][colIdx]}${rows[1][colIdx]}${rows[2][colIdx]}${rows[3][colIdx]}';
+  return int.tryParse(intStr.trim());
+}
+
+int opSum(int a, int b) => a + b;
+int opProduct(int a, int b) => a * b;
 
 int partOne(List<String> input) {
   final seperatorRe = RegExp(' +');
@@ -20,9 +60,7 @@ int partOne(List<String> input) {
   for (var colIdx = 0; colIdx < rows.first.length; ++colIdx) {
     final op = opRow[colIdx];
 
-    final opFunc = op == '+'
-        ? ((int sum, int b) => sum + b)
-        : ((int product, int b) => product * b);
+    final opFunc = op == '+' ? opSum : opProduct;
 
     solution += rows.map((x) => int.parse(x[colIdx])).reduce(opFunc);
   }
